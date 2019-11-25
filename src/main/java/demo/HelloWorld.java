d0f6318234ece4bff71fc8e5a787ffcc6b576ca6
Jonathan Lazzara <jon@nuvalence.io>
2019-11-25 11:45:58 -0500

    Initial population

---
+++ src/main/java/demo/HelloWorld.java
@@ -0,0 +1,35 @@
+package demo;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Optional;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> {
+            String msg = String.format("Hello %s", Optional.ofNullable(System.getenv("WORLD_NAME")).orElse("World"));
+            System.out.println(msg);
+            return msg;
+        });
+        get("/cache", (req, res) -> {
+            Jedis jedis = null;
+            try {
+                jedis = new Jedis(System.getenv("REDIS_HOST"), Integer.parseInt(System.getenv("REDIS_PORT")));
+                jedis.connect();
+                System.out.println("Connected to redis!");
+                return "Connected to redis!";
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Unable to connect to redis :(");
+                return "Unable to connect to redis :(";
+            } finally {
+                if (jedis != null && jedis.isConnected()) {
+                    jedis.disconnect();
+                }
+            }
+        });
+    }
+}
+
