591dac05bd748e4d2b18d7da4592ebfd74baf603
Jonathan Lazzara <jon@nuvalence.io>
2019-12-10 23:51:24 -0500

    very rough version of excelsior workshop 2 on arch evolution

---
+++ src/main/java/io/nuvalence/workshops/DocumentAPI.java
@@ -0,0 +1,111 @@
+package io.nuvalence.workshops;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.StringInputStream;
+import com.amazonaws.util.json.Jackson;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.InputStream;
+import java.util.*;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class DocumentAPI {
+
+    private final AmazonDynamoDB dynamo;
+    private final AmazonS3 s3Client;
+    private final String tableName;
+    private final String bucketName;
+
+    public static void run() {
+        get("/", (req, res) -> {
+            return "ok";
+        });
+        post("/document", (req, res) -> {
+            try {
+                Map<String, String> body = new ObjectMapper().readValue(req.body(), new TypeReference<Map<String, String>>() {
+                });
+                return new DocumentAPI().store(body.get("content")).toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "failed";
+            }
+        });
+    }
+
+    public DocumentAPI() {
+        // Create a new instance of the S3 client, make sure to specify the region
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion("us-east-1")
+                .build();
+
+        // Create a new instance of the DynamoDB client
+        dynamo = AmazonDynamoDBClientBuilder.defaultClient();
+
+        // Two environment variables must be set, the first is BUCKET_NAME, the second is TABLE_NAME
+        // The variables can be set using the shell export command
+        bucketName = System.getenv("BUCKET_NAME");
+        tableName = System.getenv("TABLE_NAME");
+    }
+
+    public UUID store(String documentContent) throws Exception {
+        // Create the key information we want to store
+        final long time = System.currentTimeMillis(); // the current time in milliseconds
+        final UUID userGUID = UUID.randomUUID(); // a user GUID we can store in Dynamo
+        final String objectKey = "document/" + time + ".txt"; // an object key to use for storing our S3 object
+
+        // Create a new metadata object for our S3 request (NOTE - this is different from our document metadata which is a business requirement)
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType("plain/text"); // set the content type
+        metadata.addUserMetadata("x-amz-meta-title", "Hello"); // set a title for the file
+
+        // Create InputStream object representing the content of our file
+        InputStream stream = new StringInputStream(documentContent);
+
+        // Create a put object request and use the S3 client to put our object into S3 at the specified key
+        PutObjectRequest request = new PutObjectRequest(bucketName, objectKey, stream, metadata);
+        s3Client.putObject(request);
+
+        System.out.println("Successfully put object in S3 at key " + objectKey);
+
+        // Start building our item we want to insert into DynamoDB
+        HashMap<String, AttributeValue> item = new HashMap<>();
+
+        //Insert values into item
+        item.put("associatedUser", new AttributeValue(userGUID.toString()));
+        item.put("objectKey", new AttributeValue(objectKey));
+        item.put("date", new AttributeValue().withN(Long.toString(time)));
+
+        //Put item in DynamoDB table
+        System.out.println("Adding new item...");
+        this.dynamo.putItem(this.tableName, item);
+        System.out.println("Success! Added item to Dynamo for user " + userGUID + " and object key " + objectKey);
+
+        // Print out the current state of both the bucket and the table
+        System.out.println("Current bucket contents:");
+        ListObjectsV2Result result = s3Client.listObjectsV2(bucketName);
+        result.getObjectSummaries()
+                .stream()
+                .map(S3ObjectSummary::getKey)
+                .forEach(System.out::println);
+
+        System.out.println("Current table contents:");
+        ScanResult dbresult = dynamo.scan(tableName, Arrays.asList("associatedUser", "objectKey", "date"));
+        dbresult.getItems().stream()
+                .map(m -> String.format("%s - %s - %s", m.get("associatedUser").getS(), m.get("objectKey").getS(), m.get("date").getN()))
+                .forEach(System.out::println);
+
+        return userGUID;
+    }
+}
---
+++ workshops/arch-evol-2/README.md
@@ -0,0 +1,71 @@
+# Document Storage using S3 and Dynamo Workshop
+In this workshop, we will deploy an API to ECS and proxy to the endpoint in the container
+using API Gateway.
+
+## Pushing image to ECR
+1. Create a new ECR repository named {your name}/workshop-api
+2. Follow the push commands to build and push your image to ECR
+
+## Creating a Task Definition
+1. In ECS, click Task Definitions
+2. Click create new and select Fargate
+3. Name the definition "nuvalence-docs-app"
+4. Select the ecsTaskExecutionRole
+configure memory and cpu
+add a container
+name the container "docs-api"
+specify the image 
+add a port mapping for 4567
+click create
+
+## Creating the ALB
+go to EC2
+create new load balancer
+select app load balancer
+name
+internet facing
+ipv4
+create a new SG
+name excelsior-alb-sg
+allow tcp on 80
+new target group
+name excelsior-tg
+ip
+http protocol
+port 4567
+skip register targets
+create
+
+## Creating a service
+for your task def, click actions -> create service
+launch type fargate
+service name "{yourname}-docs-service"
+1 task for num tasks
+click edit button on security group
+allow custom tcp on 4567 from anywhere
+select application load balancer
+select lb created
+select tg created
+uncheck the service discovery check box
+
+## Attaching policies for S3 and Dynamo Access
+Attach AmazonDynamoDBFullAccess and AmazonS3FullAccess to the ecsTaskExecution role
+
+test the endpoint through the alb
+curl -XPOST http://excelsior-alb-1591154216.us-east-1.elb.amazonaws.com/document -d '{"content":"Hello ECS"}'
+
+## Creating API Gateway
+create new regional api with name {}
+create a new resource
+check box for proxy resource
+enter the url for the lb followed by /{proxy}
+click actions
+deploy api
+[New Stage]
+stage name dev
+use the invoke url to test your endpoint
+curl -XPOST https://j0c34dnqd9.execute-api.us-east-1.amazonaws.com/dev/document -d '{"content":"Hello API Gateway"}'
+
+
+
+ 
